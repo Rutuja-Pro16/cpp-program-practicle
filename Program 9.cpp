08b581ca607fd0831c3699c64533232e4f9202db
Rutuja-Pro16 <78415221+Rutuja-Pro16@users.noreply.github.com>
2021-02-04 11:39:15 +0530

    Add files via upload

---
+++ Program 9.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+using namespace std;
+float cylinder() {
+	float v, r=2, h=5;
+	v = 3.14 * r * r * h;
+	return (v);
+}
+int main() {
+	float z = cylinder();
+	cout << "Cylinder Volume : " << z << endl; // prints !!!Hello World!!!
+	return 0;
+}
