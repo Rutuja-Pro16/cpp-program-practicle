0bc08113c6e140479e72e5019fa742c863be6702
Rutuja-Pro16 <78415221+Rutuja-Pro16@users.noreply.github.com>
2021-02-04 12:13:55 +0530

    Add files via upload

---
+++ Program 13.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+using namespace std;
+int rectangle() {
+	int l = 500, b = 300, r;
+	r = l * b;
+	return (r);
+}
+int main() {
+	int z = rectangle();
+	cout << "Area of Rectangle : " << z << endl; 
+	return 0;
+}
