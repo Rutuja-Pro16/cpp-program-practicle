12f0424b45a577cb059df3ed2be8735eaf7ee86c
Rutuja-Pro16 <78415221+Rutuja-Pro16@users.noreply.github.com>
2021-02-04 11:44:41 +0530

    Add files via upload

---
+++ Program 10.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+using namespace std;
+int add() {
+	int a = 2, b = 2;
+	int c = a + b;
+	return (c);
+}
+int main() {
+	int r = add();
+	cout << "The value of r is : " << r << endl;
+}
+
