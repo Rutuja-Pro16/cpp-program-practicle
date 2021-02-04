7e235140904486e44c509fb49ba214cf96de0ee2
Rutuja-Pro16 <78415221+Rutuja-Pro16@users.noreply.github.com>
2021-02-04 11:15:50 +0530

    Add files via upload

---
+++ Program 5.cpp
@@ -0,0 +1,13 @@
+
+
+#include <iostream>
+using namespace std;
+int sub() {
+	int x = 10, y = 5;
+	int c = x - y;
+	return (c);
+}
+int main() {
+	int c = sub();
+	cout << "The sub is : " << c << endl;
+}
