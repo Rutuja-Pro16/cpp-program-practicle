d772e62b9a93bffd87722083e1ccf0181dc3d749
Rutuja-Pro16 <78415221+Rutuja-Pro16@users.noreply.github.com>
2021-02-04 11:51:36 +0530

    Add files via upload

---
+++ Program 11.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+using namespace std;
+int div() {
+	int x = 10, y = 5;
+	int r = x / y;
+	return (r);
+}
+int main() {
+	int r = div();
+	cout << "The division is : " << r << endl;
+	return 0;
+}
