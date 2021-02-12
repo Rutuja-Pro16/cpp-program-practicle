07502791223a46a640b811fb70c99a67ea7ceb8a
Rutuja-Pro16 <78415221+Rutuja-Pro16@users.noreply.github.com>
2021-02-12 13:39:29 +0530

    without virtua

---
+++ wovirtual.cpp
@@ -0,0 +1,45 @@
+//============================================================================
+// Name        : wovirtual.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+
+#include <iostream>
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+using namespace std;
+class demo
+{
+public:
+ void dis()
+	{
+		cout<<"class demo";
+	}
+};
+class demo2:public demo
+{
+public:
+	void dis()
+	{
+		cout<<"class demo2";
+
+	}
+};
+
+
+int main() {
+	demo d1,*d1pt;
+	d1pt=&d1;
+	d1pt->dis();
+	demo2 d2;
+	d1pt=&d2;
+	d1pt->dis();
+
+
+	return 0;
+}
+
