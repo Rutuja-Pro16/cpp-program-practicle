3bb420eb0e83142cccb7fe2b769a33adf9dd22fd
Rutuja-Pro16 <78415221+Rutuja-Pro16@users.noreply.github.com>
2021-02-16 22:20:08 +0530

    pattern4

---
+++ sample4.cpp
@@ -0,0 +1,48 @@
+//============================================================================
+// Name        : sample4.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+class demo3{
+public:
+	int lines = 3;
+	int charcount = 1;
+	char value = 'A';
+	int spacecount=2;
+public:
+	void fun() {
+		for (int i = 1; i <= lines; i++) {
+			for (int j = 1; j <= spacecount; j++)
+			{
+				cout<<" ";
+
+			}
+			for (int k = 1; k <= charcount; k++) {
+				cout <<value;
+
+
+
+			}
+			cout << endl;
+			value++;
+
+			charcount++;
+			spacecount--;
+
+		}
+	}
+
+};
+
+int main() {
+	demo3 d3;
+	d3.fun();
+
+	return 0;
+}
