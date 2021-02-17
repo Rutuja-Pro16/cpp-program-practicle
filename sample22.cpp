f9f10767cffd3fc240ee452191adc3aed5e8c20b
Rutuja-Pro16 <78415221+Rutuja-Pro16@users.noreply.github.com>
2021-02-17 23:41:20 +0530

    4

---
+++ sample22.cpp
@@ -0,0 +1,38 @@
+//============================================================================
+// Name        : sample22.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+int lines=4;
+	int numbercount=7;
+	int value=1;
+	int spacecount=1;
+	for(int i=1;i<=lines;i++)
+	{
+		for(int j=1;j<=spacecount;j++)
+		{
+			cout<<" ";
+		}
+		for(int k=1;k<=numbercount;k++)
+		{
+			cout<<value;
+			value++;
+
+		}
+		cout<<endl;
+		value=1;
+		spacecount++;
+		numbercount=numbercount-2;
+	}
+
+
+}
