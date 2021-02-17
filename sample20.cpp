e21cefded0945e04eb4fc833eee438bd1d072cec
Rutuja-Pro16 <78415221+Rutuja-Pro16@users.noreply.github.com>
2021-02-17 23:16:27 +0530

    2

---
+++ sample20.cpp
@@ -0,0 +1,38 @@
+//============================================================================
+// Name        : sample20.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+int lines=4;
+	int charcount=1;
+	char value='A';
+	int spacecount=3;
+	for(int i=1;i<=lines;i++)
+	{
+		for(int j=1;j<=spacecount;j++)
+		{
+			cout<<" ";
+		}
+		for(int k=1;k<=charcount;k++)
+		{
+			cout<<value;
+			value++;
+
+		}
+		cout<<endl;
+		value='A';
+		spacecount--;
+		charcount=charcount+2;
+	}
+
+
+}
