31ebce351a1d7a673d4a3d36f2cf83d236cc136d
Rutuja-Pro16 <78415221+Rutuja-Pro16@users.noreply.github.com>
2021-02-18 00:13:56 +0530

    7

---
+++ sample25.cpp
@@ -0,0 +1,32 @@
+//============================================================================
+// Name        : sample25.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int lines=4;
+	int starcount=7;
+
+	int spacecount=1;
+	for(int i=1;i<=lines;i++)
+	{
+		for(int j=1;j<=spacecount;j++)
+		{
+			cout<<" ";
+		}
+		for(int k=1;k<=starcount;k++)
+		{
+			cout<<"*";
+		}
+		cout<<endl;
+		spacecount++;
+		starcount=starcount-2;
+	}
+}
